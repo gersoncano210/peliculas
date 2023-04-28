1944cb786b2bbeed0a5eb1d6c22adf4637d55b69
Usuario <Usuario@DESKTOP-0UB62RU>
2023-04-27 19:04:53 -0500

    Agregar archivos de proyecto.

---
+++ peliculas/Data/peliculasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using peliculas.Models;
+
+namespace peliculas.Data
+{
+    public class peliculasContext : DbContext
+    {
+        public peliculasContext (DbContextOptions<peliculasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<peliculas.Models.movie> movie { get; set; } = default!;
+    }
+}
